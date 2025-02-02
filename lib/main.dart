287b7e02d0057a10008586100c41f4065a9608ff
RobertPlavka <robert.plavka@gmail.com>
2025-02-02 19:30:31 +0100

    Project upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:roll_dice/pages/pageExpense/pageExpenses.dart';
-import 'package:roll_dice/pages/pageLibrary/pageLibrary.dart';
-
-var kColorScheme = ColorScheme.fromSeed(seedColor: const Color(0xFF2b2d42));
-var kDarkColorScheme = ColorScheme.fromSeed(
-    seedColor: const Color.fromARGB(255, 5, 99, 125),
-    brightness: Brightness.dark);
+import 'package:roll_dice/pages/pageRNG/pageRNG.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: kDarkColorScheme,
-        cardTheme: const CardTheme().copyWith(
-          color: kDarkColorScheme.secondaryContainer,
-          elevation: 1,
-          margin:
-              const EdgeInsets.only(top: 10, left: 20, right: 20, bottom: 0),
-        ),
-      ),
-      theme: ThemeData().copyWith(
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
-          elevation: 2,
-        ),
-        cardTheme: const CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          elevation: 1,
-          margin:
-              const EdgeInsets.only(top: 10, left: 20, right: 20, bottom: 0),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
-          ),
-        ),
-        textTheme: const TextTheme().copyWith(
-          titleLarge: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: kColorScheme.onSecondaryContainer,
-            fontSize: 20,
-          ),
-        ),
-        colorScheme: kColorScheme,
-      ),
+    const MaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
-      home: PageExpenses(),
+      home: PageRNG(),
     ),
   );
 }
